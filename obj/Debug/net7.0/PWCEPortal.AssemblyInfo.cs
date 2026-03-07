d99f3dda104f10f7f1e21f4ab21902e519aa3a38
abnite <abnite@gmail.com>
2026-03-07 23:46:40 +0000

    UPDATE the View Badge Issue

--- obj/Debug/net7.0/PWCEPortal.AssemblyInfo.cs
+++ obj/Debug/net7.0/PWCEPortal.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PWCEPortal")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0984938fb8e5f38eb0f677103fbb36921ff16450")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("PWCEPortal")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PWCEPortal")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
